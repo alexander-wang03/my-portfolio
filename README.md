0b33a20bf365a6e053e663e262daecfd78dbe976
Alexander Wang <87671725+alexander-wang03@users.noreply.github.com>
2024-12-20 18:17:28 -0500

    Fixed lint issues

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Welcome to my portfolio website! This project showcases my personal and professional work as a developer and serves as a hub for my projects, skills, and contact information.
 
 ## Tech Stack
-- **React** with **TypeScript**
-- **Next.js** (App Router)
+- **React** 
+- **Next.js**
 - **Tailwind CSS**
-- Hosted on **Vercel**
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">"No one's going to build a robot just for the hell of it." -Christopher Nolan</h1>
+      <h1 className="text-4xl font-bold">
+        &quot;No one&apos;s going to build a robot just for the hell of it.&quot; -Christopher Nolan
+      </h1>
     </div>
   );
 }
