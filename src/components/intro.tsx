33b7ef78a68d2ec81f87869efcc87389e27f4d48
Alexander Wang <87671725+alexander-wang03@users.noreply.github.com>
2025-01-01 20:31:03 -0500

    Fix CV download bug

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -56,12 +56,11 @@ export default function Intro() {
 
             <a
               className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 w-full sm:w-auto"
-              href="YOUR_HOSTED_CV_URL_HERE"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="/CV.pdf"
+              download
             >
-              View CV{" "}
-              <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition" />
+              Download CV{" "}
+              <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
             </a>
 
             <div className="flex flex-row gap-2">
