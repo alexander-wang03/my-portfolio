7e142d61f323f2277b31bd7c93bd74c4c5851037
Alexander Wang <87671725+alexander-wang03@users.noreply.github.com>
2025-01-01 20:29:36 -0500

    Fixed intro section mobile display

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -21,7 +21,7 @@ export default function Intro() {
       id="home"
       className="mb-28 max-w-[80rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className="flex items-center justify-between gap-8">
+      <div className="flex flex-col-reverse items-center justify-center gap-8 sm:flex-row sm:justify-between">
         <div className="flex-1 max-w-[50rem]">
           <motion.h1
             className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
@@ -35,7 +35,7 @@ export default function Intro() {
           </motion.h1>
 
           <motion.div
-            className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+            className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
@@ -44,7 +44,7 @@ export default function Intro() {
           >
             <Link
               href="#contact"
-              className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+              className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition w-full sm:w-auto"
               onClick={() => {
                 setActiveSection("Contact");
                 setTimeOfLastClick(Date.now());
@@ -55,33 +55,36 @@ export default function Intro() {
             </Link>
 
             <a
-              className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-              href="/CV.pdf"
-              download
+              className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 w-full sm:w-auto"
+              href="YOUR_HOSTED_CV_URL_HERE"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Download CV{" "}
-              <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+              View CV{" "}
+              <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition" />
             </a>
 
-            <a
-              className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-              href="https://www.linkedin.com/in/alexander-wang03/"
-              target="_blank"
-            >
-              <BsLinkedin />
-            </a>
+            <div className="flex flex-row gap-2">
+              <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://www.linkedin.com/in/alexander-wang03/"
+                target="_blank"
+              >
+                <BsLinkedin />
+              </a>
 
-            <a
-              className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-              href="https://github.com/alexander-wang03"
-              target="_blank"
-            >
-              <FaGithubSquare />
-            </a>
+              <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com/alexander-wang03"
+                target="_blank"
+              >
+                <FaGithubSquare />
+              </a>
+            </div>
           </motion.div>
         </div>
 
-        <div className="relative flex-shrink-0">
+        <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
